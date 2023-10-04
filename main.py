96dcd09739fa156498696978e757c93137c0e401
T-Sugiyama-dev <takumasa.aran@gmail.com>
2023-10-05 00:01:39 +0900

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+from flask import Flask
+from app.router.router import init_app_routes
+
+app = Flask(__name__)
+
+init_app_routes(app)
+
+if __name__ == '__main__':
+  app.run(debug=True, port=8080)
